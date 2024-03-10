0418fb714a96946e3b6d6f4e2cb5c88ab104195a
gayathripittu <48981111+gayathripittu@users.noreply.github.com>
2024-03-09 23:56:51 -0500

    Add files via upload

---
+++ passenger_queries.sql
@@ -0,0 +1,11 @@
+select * from "passenger_App_passenger";
+
+
+
+INSERT INTO "passenger_App_passenger"("passenger_ID", "name", "age", "gender", "mobile_number", "booking_details") 
+VALUES
+(1, 'John Smith', 35, 'M', 'john@example.com', 'Seat 12A'),
+(2, 'Emily Brown', 28, 'F', 'emily@example.com', 'Seat 8C'),
+(3, 'Alex Johnson', 42, 'M', 'alex@example.com', 'Seat 15B'),
+(4, 'Jessica Lee', 19, 'F', 'jessica@example.com', 'Seat 3D'),
+(5, 'Taylor Davis', 31, 'O', 'taylor@example.com', 'Seat 7F');
